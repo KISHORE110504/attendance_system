b319c2a69e0fa3627a47ec13e6b113321ef08b4c
KISHORE A <90239515+KISHORE110504@users.noreply.github.com>
2024-06-18 22:51:46 +0530

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -37,11 +37,12 @@ class Face_recognition:
         f_lbl.place(x=650, y=45, width=950, height=screen_height)
         
         #button
-        b1 = Button(f_lbl, text = "FACE RECOGNITION", cursor="hand2", font=("times new roman", 13, "bold"), bg="darkgreen", fg="white")
+        b1 = Button(f_lbl, text = "FACE RECOGNITION", cursor="hand2", command=self.face_recog, font=("times new roman", 13, "bold"), bg="darkgreen", fg="white")
         b1.place(x = 350, y = 250, width =200, height = 40)
-        
+
+                   
     #face recognition
-    def face_recog(self):
+    def face_recog(self): #main function
         def draw_boundry(img, classifier, scaleFactor, minNeighbors, color, text, clf):
             gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             features = classifier.detectMultiScale(gray_image, scaleFactor, minNeighbors)
@@ -49,19 +50,59 @@ class Face_recognition:
             coord = []
             
             for (x,y,w,h) in features:
-                cv2.rectangle(img(x,y),(x+w,y+h), (0,255,0), 3)
+                cv2.rectangle(img, (x,y),(x+w,y+h), (0,255,0), 3)
                 id,predict = clf.predict(gray_image[y:y+h,x:x+w])
                 confidence = int((100*(1-predict/300)))
                 
-                if confidence > 77:
-                    
+                conn = mysql.connector.connect(host="localhost", user="root", password="Kishore@1105", database="face_recognizer", auth_plugin ='mysql_native_password')
+                my_cursor = conn.cursor()
                 
+                my_cursor.execute("Select Name from student where Student_id="+str(id))
+                n = my_cursor.fetchone()
+                n = "+".join(n)
+                
+                my_cursor.execute("Select Roll from student where Student_id="+str(id))
+                r = my_cursor.fetchone()
+                r = "+".join(r)
+                
+                my_cursor.execute("Select Dep from student where Student_id="+str(id))
+                d = my_cursor.fetchone()
+                d = "+".join(d)
+                
+                if confidence > 77:
+                    cv2.putText(img, f"Roll: {r}", (x,y-55), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255,255,255), 3)
+                    cv2.putText(img, f"Name: {n}", (x,y-30), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255,255,255), 3)
+                    cv2.putText(img, f"Department: {d}", (x,y-5), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255,255,255), 3)
+                
+                else:
+                    cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 3)
+                    cv2.putText(img, "Unknown Face", (x,y-5), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255,255,255), 3)
+                
+                coord = [x,y,w,h]
+            return coord
+        
+        def recognize(img, clf, faceCascade):
+            coord = draw_boundry(img, faceCascade, 1.1, 10, (255,255,255), "Face", clf)
+            return img
+        
+        faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")    
+        clf = cv2.face.LBPHFaceRecognizer_create()
+        clf.read("classifier.xml")
+        
+        video_capture = cv2.VideoCapture(0)        
+        
+        while True:
+            ret, img = video_capture.read()
+            img = recognize(img, clf, faceCascade)
+            cv2.imshow("Welcome to Face Recognition", img)
+                
+            if cv2.waitKey(1) == 13:
+                break    
+
+        video_capture.release()
+        cv2.destroyAllWindows()
         
         
-        
-
-
-
 if __name__ == "__main__":
     root = Tk()
     obj = Face_recognition(root)

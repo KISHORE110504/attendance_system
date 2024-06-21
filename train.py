bd3886805718c6c93a9a8f304ce19da53429472d
KISHORE A <90239515+KISHORE110504@users.noreply.github.com>
2024-06-21 23:33:01 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,43 +1,28 @@
 from tkinter import *
 from tkinter import ttk
-from PIL import Image,ImageTk
+from PIL import Image, ImageTk
 from tkinter import messagebox
 import mysql.connector
 import cv2
 import os
 import numpy as np
 
-
 class Train:
-    def __init__(self,root):
+    def __init__(self, root):
         self.root = root
-        self.root.geometry("1530x790+0+0")
+        self.root.geometry("600x200")
         self.root.title("Face Recognition System")
-        screen_width = self.root.winfo_screenwidth()
-        screen_height = self.root.winfo_screenheight()
-        self.root.geometry(f"{screen_width}x{screen_height}+0+0")
+        self.root.configure(bg = "black")
         
-        title_lbl = Label(self.root, text="TRAIN DATA SET", font=("times new roman", 35, "bold"), bg="white", fg="darkgreen")
-        title_lbl.place(x=0, y=0, width=screen_width, height=45)
-        
-        img_top = Image.open(r"Images\Facial-Recognition-System.png")
-        img_top = img_top.resize((screen_width,325), Image.LANCZOS)
-        self.photoimg_top = ImageTk.PhotoImage(img_top)
-        
-        f_lbl = Label(self.root, image=self.photoimg_top)
-        f_lbl.place(x=0, y=45, width=screen_width, height=325)
-        
-        #button
-        b1 = Button(self.root, text = "TRAIN DATA", command=self.train_classifier, cursor="hand2", font=("times new roman", 30, "bold"), bg="darkgreen", fg="white")
-        b1.place(x = 0, y = 370, width =screen_width, height = 70)
-        
-        img_bottom = Image.open(r"Images\Facial-Recognition-System.png")
-        img_bottom = img_bottom.resize((screen_width,325), Image.LANCZOS)
-        self.photoimg_bottom = ImageTk.PhotoImage(img_bottom)
-        
-        f_lbl = Label(self.root, image=self.photoimg_bottom)
-        f_lbl.place(x=0, y=440, width=screen_width, height=325)
+        # Main window title
+        title_lbl = Label(self.root, text="TRAIN DATA SET", font=("times new roman", 25, "bold"), bg="white", fg="darkgreen")
+        title_lbl.pack(side=TOP, fill=X)
     
+        
+        # Button to open the training popup
+        open_popup_btn = Button(self.root, text="TRAIN DATA", command=self.train_classifier, cursor="hand2", font=("times new roman", 15, "bold"), bg="darkgreen", fg="white")
+        open_popup_btn.pack(pady=50)
+
     def train_classifier(self):
         data_dir = ("data")
         path = [os.path.join(data_dir, file) for file in os.listdir(data_dir)]
@@ -62,22 +47,7 @@ class Train:
         clf.write("classifier.xml")
         cv2.destroyAllWindows()
         messagebox.showinfo("Result","Training datasets completed!")
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        pass
 
 if __name__ == "__main__":
     root = Tk()
